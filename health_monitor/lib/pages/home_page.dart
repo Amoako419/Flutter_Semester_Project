c4d0f43c9ccfc7e77e71c5908d6f22cdb21d2328
amoako419 <ghheskey@gmail.com>
2024-03-11 15:11:50 +0000

    Another commit

---
+++ health_monitor/lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  Homepage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<Homepage> {
+  _HomePageState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
