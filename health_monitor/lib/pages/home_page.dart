5244fba98247433329db97d9421daf64b260a204
amoako419 <ghheskey@gmail.com>
2024-03-20 01:38:32 +0000

    Profile Page Done

--- health_monitor/lib/pages/home_page.dart
+++ health_monitor/lib/pages/home_page.dart
@@ -346,26 +346,85 @@ class Profile extends StatelessWidget{
 body: Column(children: [
     _avatar(),
     _ProfileInfo(),
+    _edit(),
 ]),
     );
   }
 
   // ignore: non_constant_identifier_names
   Widget _ProfileInfo(){
-return const Column(
-  children: [
-    Text("Name: Steve Jobs")
-  ],
+return Form(
+  child: Column(
+    children: [
+      TextFormField(
+        readOnly: true,
+        initialValue: 'Steve Jobs',
+        decoration: const InputDecoration(
+          labelText: 'Name',
+        ),
+      ),
+      
+      TextFormField(
+         readOnly: true,
+        initialValue: 'steve.jobs@icloud.com',
+        decoration: const InputDecoration(
+          labelText: 'Email',
+        ),
+      ),
+
+      TextFormField(
+         readOnly: true,
+        initialValue: '55' ,
+        decoration: const InputDecoration(
+          labelText: 'Age',
+        ),
+      ),
+
+       TextFormField(
+          readOnly: true,
+        initialValue: '3-10-1979',
+        decoration: const InputDecoration(
+          labelText: 'Date of Birth',
+        ),
+      ),
+
+       TextFormField(
+          readOnly: true,
+        initialValue: '68kg',
+        decoration: const InputDecoration(
+          labelText: 'Weight',
+        ),
+      ),
+       TextFormField(
+          readOnly: true,
+        initialValue: '68cm',
+        decoration: const InputDecoration(
+          labelText: 'Height',
+        ),
+      ),
+    ],
+  ),
 );
 }
 
 Widget _avatar(){
-  return const Center(
+  return const Column( 
+    children:[ Center(
     child: CircleAvatar(
       radius: 95.0,
       backgroundImage: 
       AssetImage('assets/images/jobs.JPG'),
       ),
+  ),Text("change picture", style: TextStyle(color: Colors.blue,))
+  ]
+  );
+}
+
+Widget _edit(){
+  return ElevatedButton(onPressed: (){}, 
+  style: ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(Colors.green)
+  ),child: const Text("Edit details")
   );
 }
 }
