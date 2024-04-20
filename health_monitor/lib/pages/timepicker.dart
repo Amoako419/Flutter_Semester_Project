cba4486fa05493fcd19e02d516d2f9da7be78cc5
amoako419 <ghheskey@gmail.com>
2024-04-20 02:23:08 +0000

    initial commit

--- health_monitor/lib/pages/timepicker.dart
+++ health_monitor/lib/pages/timepicker.dart
@@ -1,5 +1,5 @@
 
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TimePickerWidget extends StatefulWidget {
@@ -7,6 +7,7 @@ class TimePickerWidget extends StatefulWidget {
 
     @override
     State<TimePickerWidget> createState() => _TimePickerWidgetState();
+     
 }
 
 class _TimePickerWidgetState extends State<TimePickerWidget> {
@@ -37,7 +38,7 @@ class _TimePickerWidgetState extends State<TimePickerWidget> {
         });
         }
     });}
-
+      bool switchValue = true;
   
     @override
     Widget build(BuildContext context) {
@@ -69,18 +70,60 @@ class _TimePickerWidgetState extends State<TimePickerWidget> {
         color: Color.fromARGB(255, 230, 221, 221)),
         child: Column(
           children: [
-             Text('From: ${_time.hour}:${_time.minute}',style: const TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold),),
-        ElevatedButton(
+             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                 Text('From: ${_time.hour}:${_time.minute}',style: const TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold),),ElevatedButton(
             onPressed: _showTimePicker,
             child: const Text('Pick Time'),
         ),
+               ],
+             ),
+        
         Padding(padding: EdgeInsets.all(10)),
-              Text('To: ${_time2.hour}:${_time2.minute}',style: const TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold),),
-        ElevatedButton(
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('To: ${_time2.hour}:${_time2.minute}',style: const TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold),),  ElevatedButton(
             onPressed: _TimePicker,
             child: const Text('Pick Time'),
         ),
-        
+                ],
+              ),
+        const Padding(padding: EdgeInsets.all(10)),
+         
+         Row(
+           children: [
+            
+             CupertinoSwitch(
+              // This bool value toggles the switch.
+              value: switchValue,
+              activeColor: CupertinoColors.activeBlue,
+              onChanged: (bool? value) {
+                // This is called when the user toggles the switch.
+                setState(() {
+                  switchValue = value ?? false;
+                });
+              },
+                     ),
+            const Text("Notify me"),
+           ],
+         ),
+
+         ElevatedButton(
+    style: ElevatedButton.styleFrom(
+    primary: Colors.blue,
+    onPrimary: Colors.white,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+    ),
+    ),
+    onPressed: () {},
+    child: const Text('Save'),
+)
+
+
           ],
         ),
       );
